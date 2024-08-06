5d6dc818b3d65a08f0b6ef692ccec3bc58ce2035
gamotapcode <dinhyenphan@gmail.com>
2024-08-06 18:00:01 +0700

    Thêm Popper

---
+++ src/components/Layout/AccountItem/index.js
@@ -0,0 +1,30 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://media.istockphoto.com/id/517188688/vi/anh/phong-c%E1%BA%A3nh-n%C3%BAi-non.jpg?s=612x612&w=0&k=20&c=WWWaejSo6EWGZMZSK7QK6LCfwd0rL2KB3ImCX2VkW4A="
+        alt="Thúy"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Phạm Thanh Thúy</span>
+          <FontAwesomeIcon
+            className={cx('check')}
+            icon={faCheckCircle}
+          />
+        </h4>
+        <span className={cx('username')}> abc</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
